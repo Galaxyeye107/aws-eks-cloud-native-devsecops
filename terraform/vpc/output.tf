1deaa76fb3f9281f4878217c12fa13342cd0a616
Trung_DevSecOps <gomcong@hoahue.io.vn>
2026-03-16 16:34:44 +0700

    feat: export vpc outputs for cross-module communication

--- terraform/vpc/output.tf
+++ terraform/vpc/output.tf
@@ -1,8 +1,15 @@
-# File: terraform/vpc/outputs.tf
+# Đây là "bảng tin" công khai của VPC
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  description = "ID của VPC để các dịch vụ khác (EKS, RDS) sử dụng"
+  value       = module.vpc.vpc_id
 }
 
 output "private_subnets" {
-  value = module.vpc.private_subnets
+  description = "Danh sách các Private Subnets để chạy EKS Nodes (Bảo mật)"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "Danh sách các Public Subnets để đặt Load Balancer"
+  value       = module.vpc.public_subnets
 }
