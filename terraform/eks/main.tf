56eeef6b1a0cd939f2a98b8eb3caebdae4385edb
Trung_DevSecOps <gomcong@hoahue.io.vn>
2026-03-28 21:05:18 +0700

    add ec2 for mysql open node

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -67,8 +67,8 @@ module "eks" {
   eks_managed_node_groups = {
     main = {
       min_size       = 1
-      max_size       = 4
-      desired_size   = 4            # Cấu hình đủ để chạy lab.
+      max_size       = 5
+      desired_size   = 5            # Cấu hình đủ để chạy lab.
       instance_types = ["t3.micro"] # Đủ để chạy lab, đừng dùng t3.micro vì sẽ thiếu RAM
       capacity_type  = "ON_DEMAND"  # tài khoản free tier không có spot instance, nếu có thể thì dùng SPOT để tiết kiệm chi phí
     }
