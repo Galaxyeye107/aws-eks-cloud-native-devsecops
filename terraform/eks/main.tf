b1a8e9a20f0863d0145cb1fc65a50e95be146a74
Trung_DevSecOps <gomcong@hoahue.io.vn>
2026-03-29 02:26:04 +0700

    feat(terraform): add metrics-server using helm provider 2

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -25,13 +25,14 @@ provider "kubernetes" {
 
 # 1. Khai báo Provider Helm
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
+
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
   }
 }
