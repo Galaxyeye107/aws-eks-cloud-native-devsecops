3b07024608575957e86f19a95b45847abb3a364d
Trung_DevSecOps <gomcong@hoahue.io.vn>
2026-03-29 02:49:30 +0700

    Fix: desires_size=2

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -88,7 +88,7 @@ module "eks" {
     main = {
       min_size       = 1
       max_size       = 3
-      desired_size   = 1            # Cấu hình đủ để chạy lab.
+      desired_size   = 2            # Cấu hình đủ để chạy lab.
       instance_types = ["t3.small"] # Đủ để chạy lab, đừng dùng t3.micro vì sẽ thiếu RAM
       capacity_type  = "ON_DEMAND"  # tài khoản free tier không có spot instance, nếu có thể thì dùng SPOT để tiết kiệm chi phí
     }
